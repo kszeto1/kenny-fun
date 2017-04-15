062ac05674a18590521746358f1752bf51fbea5c
Piyush Gupta <piyushg91@gmail.com>
2017-04-15 16:03:25 -0700

    First assignment for kenny

---
+++ nba_fun/warriors.py
@@ -0,0 +1,10 @@
+
+
+roster = ('Kevin Durant', 'Klay Thompson', 'David West', 'Damian Jones', 'Javale McGee',
+          'Matt Barnes', 'Ian Clark', 'Stephen Curry', 'Draymond Green', 'Andre Iguodala', 'Shaun Livingston',
+          'Kevon Looney', 'James Michael McAdoo', 'Patrick McCaw', 'Zaza Pachulia')
+
+filename = 'lal-vs-gsw-412-quarter1.txt'
+
+# Parse the file and figure out who scored in the first quarter from the golden state team
+
