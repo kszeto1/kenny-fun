2356afb8e93284c5548a7d78a4b0be75e238ac49
Piyush Gupta <piyushg91@gmail.com>
2017-07-24 14:16:43 -0700

    New Challenge for Kenny
    Good luck!

--- nba_fun/warriors.py
+++ nba_fun/warriors.py
@@ -1,6 +1,14 @@
 import os
+import pprint
 
-# Parse the file and figure out who scored in the first quarter from the golden state team
+# Parse the file and create a dictionary of all the players mapped to how much they score.
+# Note that you'll have to handle several cases to do this correctly. I won't specify what those cases are.
+# Part of the exercise is to figure out those cases yourself.
+# Your function should return something like
+# {'Kevin Durant': 15, 'Andre Iguodala': 0}. 
+# I didn't include all the players but you get the idea. You should include any player who didn't score as having zero points.
+# A good strategy would be create a starting dictionary where every player has 0 points, then loop through the play by play string and
+# add points accordingly. 
 
 roster = ('Kevin Durant', 'Klay Thompson', 'David West', 'Damian Jones', 'JaVale McGee',
           'Matt Barnes', 'Ian Clark', 'Stephen Curry', 'Draymond Green', 'Andre Iguodala', 'Shaun Livingston',
@@ -12,16 +20,8 @@ os.chdir('D:\\Users\\Kenny\\Desktop\\kenny-fun\\nba_fun')
 print os.getcwd()
 lines = open(filename, 'r').readlines()
 
-def GSW_who_scored():
-    players_who_have_scored = []
-    for play_by_play_string in lines:
-        if "makes" in play_by_play_string:
-            for player in roster:
-                if player in play_by_play_string:
-                    if player not in players_who_have_scored:
-                        players_who_have_scored.append(player)
-                    if player in players_who_have_scored:
-                        break
-    print 'In the first quarter, the following players made a basket for Golden State: ' + str(players_who_have_scored)
+def get_roster_stats():
+    # Your code here
+    pass
 
-GSW_who_scored()
+pprint.pprint(get_roster_stats())
