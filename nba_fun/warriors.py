f13522d9b8db1521ebab858c04b857d4e87fb94c
Kenny <kenny.szeto1@gmail.com>
2017-10-25 14:23:56 -0700

    Work in progress for solution

--- nba_fun/warriors.py
+++ nba_fun/warriors.py
@@ -20,50 +20,52 @@ filename = 'lal-vs-gsw-412-quarter1.txt'
 os.chdir('D:\\Users\\Kenny\\Desktop\\kenny-fun\\nba_fun')
 print os.getcwd()
 lines = open(filename, 'r').readlines()
-# lines = '\n'.join(lines)
+lines = '\n'.join(lines)
 # print lines
 
-players_stats = {}
-for key in range(len(roster)):
-    players_stats[roster[key]] = 0
-dwest = str(roster[2])
-for key in players_stats:
-    if key == dwest:
-        players_stats[key] = 67
-        if players_stats[key] == 67:
-            players_stats[key] += 33
-    if key == roster[0]:
-        players_stats[key] += 2
-    if 'a' in key[1].lower():
-        players_stats[key] += 3
-    if 'curry' in key.lower():
-        players_stats[key] += 30
 
-thirty = []
-for e in lines:
-    digits = re.findall(r'\d+-', e)
-    print digits
+dict = {}
+for e in roster:
+    dict[e] = 0
+print dict
+
+roster1 = ('|'.join(roster))
+roster1 = '(' + roster1 + ')'
+pattern_players = re.compile(roster1)
+print roster1
+
+def get_player_scores():
+    for n in lines:
+        p = re.search(roster1, n)
+        # if p is None:
+        #     return None
+    print p.groups()
+    m = re.search("makes", lines)
+    # if p and m:
+
+            # dict[e] = 3
+            # if dict[e] > 0:
+            # dict[p] += 3
+
+# print dict
 # for e in digits:
 #     numbers = re.findall(r'\d+', digits)
 # print digits
 
-# results = map(int, numbers)
+# players_stats = {}
+# for key in range(len(roster)):
+#     players_stats[roster[key]] = 0
+# dwest = str(roster[2])
+# for key in players_stats:
+#     if key == dwest:
+#         players_stats[key] = 67
+#         if players_stats[key] == 67:
+#             players_stats[key] += 33
+#     if key == roster[0]:
+#         players_stats[key] += 2
+#     if 'a' in key[1].lower():
+#         players_stats[key] += 3
+#     if 'curry' in key.lower():
+#         players_stats[key] += 30
 
-# for numberz in results:
-#     if numberz > 25: #need to find what length three point line is
-#         thirty.append(numberz)
-
-# pprint.pprint(thirty)
-    # if "makes" in e:
-    #     print e
-
-
-
-# dict[] = 2
-# pprint.pprint(players_stats)
-
-def get_roster_stats():
-    # Your code here
-    pass
-
-pprint.pprint(get_roster_stats())
+# pprint.pprint(get_player_scores)
